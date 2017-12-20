9eb27e0aedceba7a10ab18dd37fb8fa16c34fab1
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-12-20 16:23:32 +0700

    Fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = {
-  cond: require('./lib/cond'),
-  swit: require('./lib/swit'),
-  throwErr: require('./lib/throwErr'),
-  tryCatch: require('./lib/tryCatch'),
+  createKnex: require('./lib/createKnex'),
+  createKnexAndDropAll: require('./lib/createKnexAndDropAll'),
+  defineCols: require('./lib/defineCols'),
+  dropAll: require('./lib/dropAll'),
+  dropAllAndDestroyKnex: require('./lib/dropAllAndDestroyKnex'),
+  getTables: require('./lib/getTables'),
+  upsertOne: require('./lib/upsertOne'),
 }
