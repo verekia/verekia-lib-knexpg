c689510e0885348931cf588dfc9cf524bc8f7947
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-03-10 14:37:46 +0800

    Specify tableName in where query

--- src/query/index.js
+++ src/query/index.js
@@ -1,9 +1,9 @@
 // @flow
 
-module.exports = (knex: Function, tableName: string) => (userId?: string, trx?: Function) => {
+module.exports = (knex: Function, tableName: string) => (userId?: mixed, trx?: Function) => {
   if (typeof tableName !== 'string') {
     throw Error('tableName must be a string')
   }
   const table = trx ? trx(tableName) : knex(tableName)
-  return userId ? table.where({ userId }) : table
+  return userId ? table.where(`${tableName}.userId`, userId) : table
 }
