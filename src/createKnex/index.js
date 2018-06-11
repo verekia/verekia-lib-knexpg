fcc965e9f9de57f4e15fe6c314cd199bfcb2c5ad
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-06-11 13:31:59 +0200

    Add useSsl optional param

--- src/createKnex/index.js
+++ src/createKnex/index.js
@@ -3,11 +3,11 @@
 import Knex from 'knex'
 import pg from 'pg'
 
-module.exports = (databaseUrl: string, migrationPath: string) => {
+module.exports = (databaseUrl: string, migrationPath: string, useSsl: boolean = true) => {
   if (!databaseUrl || !migrationPath) {
     throw Error('You need to pass a databaseUrl and a migrationPath to createKnex')
   }
-  pg.defaults.ssl = true
+  pg.defaults.ssl = useSsl
   return Knex({
     client: 'pg',
     connection: databaseUrl,
